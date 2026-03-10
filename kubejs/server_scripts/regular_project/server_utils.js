a5e43edbc3b54ffbd4c54987a1717cc6e486adbc
Illia <75110249+Lolens@users.noreply.github.com>
2026-03-10 22:16:49 +0300

    Add files via upload
    Signed-off-by: Illia <75110249+Lolens@users.noreply.github.com>

--- kubejs/server_scripts/regular_project/server_utils.js
+++ kubejs/server_scripts/regular_project/server_utils.js
@@ -1,1 +1,12 @@
+function broadcastMessage(text, important) {
+  let server = Utils.getServer();
+  let players = server.getPlayerList().getPlayers();
 
+  let prefix = important
+    ? "§4[ВНИМАНИЕ] "
+    : "§6[ИНФО] ";
+
+  players.forEach((player) => {
+    player.tell(prefix + text);
+  });
+}
